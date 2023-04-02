10987e3fa34dd7d3f32bfd0487e879012745bb28
Abigtime <115564855+Abigtime@users.noreply.github.com>
2023-04-02 18:26:44 +0300

    подсчет

--- Exemple001/Program.cs
+++ Exemple001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2   Hello, World!");
+﻿
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 4;
+        PrintNumbers(n);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n == 1)
+        {
+            Console.Write(n);
+            return;
+        }
+
+        Console.Write(n + ", ");
+        PrintNumbers(n - 1);
+    }
+}
